b472625a42210d5aafe19aa38dc2d8f8e1ff9703
Leonardo Vilela Cardoso <lvcardoso@sga.pucminas.br>
2026-06-05 14:01:02 -0300

    Update train.sh

--- scripts/train.sh
+++ scripts/train.sh
@@ -2,7 +2,8 @@
 
 dset_name="anet"
 data_dir="/home/lvcardoso/SkimCap/densevid_eval/${dset_name}_data"
-v_feat_dir="./video_feature/rt_anet_feat/trainval"#/cd_anet_feat"
+v_feat_dir="./video_feature/rt_anet_feat/trainval"
+# v_feat_dir="./video_feature/cd_anet_feat"
 dur_file="./video_feature/anet_duration_frame.csv"
 word2idx_path="./cache/${dset_name}_word2idx.json"
 glove_path="./cache/${dset_name}_vocab_glove.pt"
