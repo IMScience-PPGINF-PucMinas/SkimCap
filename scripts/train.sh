47af9955a7e2d6f27789985358737e2c6c032b0f
Leonardo Vilela Cardoso <lvcardoso@sga.pucminas.br>
2026-06-05 00:50:20 -0300

    Update train.sh

--- scripts/train.sh
+++ scripts/train.sh
@@ -50,8 +50,8 @@ time python src/train.py \
     --contrastive_temp 0.10 \
     --contrastive_weight 0.1 \
     --sent_loss_weight 0.15 \
-    --batch_size 32 \
-    --val_batch_size 32 \
+    --batch_size 16 \
+    --val_batch_size 16 \
     --max_es_cnt 15 \
     --num_workers 0 \
     --n_memory_cells 8 \
