a7744fa535c5550f39d11c8f3ac56d56dcd80d02
Leonardo Vilela Cardoso <lvcardoso@sga.pucminas.br>
2026-06-06 14:31:36 -0300

    Update train.sh

--- scripts/train.sh
+++ scripts/train.sh
@@ -16,7 +16,7 @@ echo ">>>>>>>> Running training on ${dset_name} dataset (CLIP + batch 128)"
 
 if [[ ${dset_name} == "anet" ]]; then
     max_n_sen=6
-    max_t_len=32
+    max_t_len=22
     max_v_len=100
 elif [[ ${dset_name} == "yc2" ]]; then
     max_n_sen=12
