7e2e16019b0916ed86cd707172a9f9a0482ea70a
Leonardo Vilela Cardoso <lvcardoso@sga.pucminas.br>
2026-06-07 00:44:04 -0300

    Update build_vocab_clip.sh

--- scripts/build_vocab_clip.sh
+++ scripts/build_vocab_clip.sh
@@ -2,7 +2,7 @@
 # Usage:
 # $ bash scripts/build_vocab_clip.sh anet
 
-python build_vocab_clip.py \
+python src/build_vocab_clip.py \
     --lang_feature_dir video_feature/lang_feature \
     --output_path      cache/anet_vocab_clip.pt \
     --clip_model       ViT-B/16
