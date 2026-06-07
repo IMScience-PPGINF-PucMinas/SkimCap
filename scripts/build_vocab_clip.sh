bfbe18ac6a791d275b8eb652dfe353d38b50a659
Leonardo Vilela Cardoso <lvcardoso@sga.pucminas.br>
2026-06-07 00:42:39 -0300

    upload files

---
+++ scripts/build_vocab_clip.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# Usage:
+# $ bash scripts/build_vocab.sh anet
+
+python build_vocab_clip.py \
+    --lang_feature_dir video_feature/lang_feature \
+    --output_path      cache/anet_vocab_clip.pt \
+    --clip_model       ViT-B/16
+
+
