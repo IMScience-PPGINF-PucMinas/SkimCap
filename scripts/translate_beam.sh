5bd29da09f81ddd058709f9837695fd0629e0cd8
Leonardo Vilela Cardoso <lvcardoso@sga.pucminas.br>
2026-06-14 16:27:10 -0300

    Update translate_beam.sh

--- scripts/translate_beam.sh
+++ scripts/translate_beam.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
-#!/usr/bin/env bash
 split_name=$1
 res_dir=$2
 checkpoint=$3
+shift 3   # consume the 3 positional args; "$@" now holds only extra flags
+
 python src/translate.py \
---res_dir=${res_dir} \
---eval_splits=${split_name} \
---checkpoint=${checkpoint} \
---use_beam \
---beam_size 2 \
-"$@"
+  --res_dir="${res_dir}" \
+  --eval_splits="${split_name}" \
+  --checkpoint="${checkpoint}" \
+  --use_beam \
+  --beam_size 2 \
+  "$@"
