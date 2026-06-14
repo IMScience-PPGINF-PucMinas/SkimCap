aa2ae5b451d7d923d1e9ca3376d368aa31546a09
Leonardo Vilela Cardoso <lvcardoso@sga.pucminas.br>
2026-06-14 16:14:27 -0300

    Update translate_beam.sh

--- scripts/translate_beam.sh
+++ scripts/translate_beam.sh
@@ -2,9 +2,11 @@
 #!/usr/bin/env bash
 res_dir=$1
 split_name=$2
+checkpoint=$3
 python src/translate.py \
 --res_dir=${res_dir} \
 --eval_splits=${split_name} \
+--checkpoint=${checkpoint}
 --use_beam \
 --beam_size 2 \
 ${@:3}
